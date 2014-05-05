330af1d021e71df87d746036ab850f458077357a
vixus0 <vixus0@gmail.com>
2014-05-05 16:27:19 +0100

    Data reading from EDF+ implemented

--- edffile.py
+++ edffile.py
@@ -3,24 +3,25 @@ from collections import namedtuple
 
 
 class BadFormatException(Exception):
+    """Raised if the file is not in the correct EDF+ format."""
     pass
-
-class 
-
-EdfHeader = namedtuple('EdfHeader',
-        'ver pid rid sdate stime nbyte reserved ndata duration nsignal'
-        )
-
-
-SignalHeader = namedtuple('SignalHeader',
-        'label ttype pdim pmin pmax dmin dmax preflt nsample reserved offset'
-        )
 
 
 class EdfFile(object):
 
     def __init__(self, filename):
         """Reads an EDF+ file from disk and returns an EdfFile object."""
+
+        # Header containers
+        EdfHeader = namedtuple('EdfHeader',
+                'ver pid rid sdate stime nbyte reserved ndata duration nsignal'
+                )
+
+        SignalHeader = namedtuple('EdfSignalHeader',
+                'label ttype pdim pmin pmax dmin dmax preflt nsample reserved offset'
+                )
+
+        Timestamp = namedtuple('EdfTime', 'hours minutes seconds')
 
         # Decode bytes to strings
         bdecode = lambda bl: [b.decode("ascii").strip() for b in bl]
@@ -48,6 +49,9 @@ class EdfFile(object):
         self.header = None
         self.signals = []
         self.filename = filename
+        self._data_record_size = 0
+        self._sigfmt = "<{}h"
+
 
         with open(filename, "rb") as f:
             # Try and read the entire header
@@ -83,35 +87,58 @@ class EdfFile(object):
                 raise BadFormatException
 
             sdata['offset'] = []
-            offset = curpos
+            offset = 0
             for ns in sdata['nsample']:
                 sdata['offset'].append(offset)
-                offset += calcsize('<{}i'.format(int(ns)))
+                offset += calcsize(self._sigfmt.format(int(ns)))
+            self._data_record_size = offset
 
             for i in range(nsig):
                 d = {k:v[i] for k,v in sdata.items()}
                 self.signals.append(SignalHeader(**d))
 
+            # Calculate total duration
+            seconds = float(self.header.duration) * int(self.header.ndata)
+            mins, secs  = divmod(seconds, 60)
+            hours, mins = divmod(mins, 60)
+            self.duration = Timestamp._make([hours, mins, secs])
 
-    def read(self, nsample, sigid, record=0):
-        """Read nsample samples of data from a signal."""
+    def read(self):
+        """Returns all data records."""
 
-        if sigid >= len(self.signals) or record > int(self.header.ndata):
-            return None
-        
-        signal = self.signals[sigid]
-
-        # Data is in integers
-        fmt = '<{}i'.format(nsample)
-        size = calcsize(fmt)
+        spos  = int(self.header.nbyte)
+        ndata = int(self.header.ndata) 
+        nsig  = len(self.signals)
+        recs  = []
+        count = 0
 
         with open(self.filename, "rb") as f:
-            print("Offset", signal.offset)
-            f.seek(signal.offset)
-            b = f.read(size)
+            f.seek(spos)
+            while True:
+                byt = f.read(self._data_record_size)
 
-        if len(b) != size:
-            raise BadFormatException
+                # Keep reading until we run out of stuff to read or we hit
+                # the number of data records specified in the header
+                count += 1
+                if ndata>0 and count==ndata:
+                    break
+                elif len(byt) < self._data_record_size:
+                    print("Warning: reached end of file. {} bytes left.".format(len(byt)))
+                    break
 
-        return unpack(fmt, b)
+                # byt is nsample * nsignal integers
+                nint = sum([s.nsample for s in self.signals])
+                fmt  = self._sigfmt.format(nint)
+                ints = unpack(fmt, byt)
+
+                dat = []
+                i   = 0
+                for s in self.signals:
+                    dat.append(ints[i:i+s.nsample])
+                    i += s.nsample
+
+                recs.append(dat)
+
+        return recs
+
 
